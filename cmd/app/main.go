c9cdfbd1162b37dacb713f7d70f574a73f51fd4f
Konstantin <snorchello@gmail.com>
2024-10-18 00:01:40 +0700

    Added feature to add users

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/dr-aw/practice/internal/app"
-	"github.com/dr-aw/practice/internal/app/dbconn"
+	"github.com/dr-aw/practice/internal/app/database"
 	"log"
 )
 
 func main() {
 	app.Run()
-	db, err := dbconn.ConnectDB()
+	db, err := database.ConnectDB()
 	if err != nil {
 		log.Fatalf("Error connect to the DataBase: %v", err)
 	}
 
 	fmt.Println("DB connected successfully:", db)
+
 }
--- internal/app/database/database.go
+++ internal/app/database/database.go
@@ -1,13 +1,20 @@
-package dbconn
+package database
 
 import (
 	"fmt"
 	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
 	"os"
 )
+
+type User struct {
+	ID            uint   `gorm:"primaryKey"`
+	Username      string `gorm:"unique;not null"`
+	Password_Hash string `gorm:"not null"`
+}
 
 // ConnectDB uses .env for connection to DB with GORM
 func ConnectDB() (*gorm.DB, error) {
@@ -33,4 +40,22 @@ func ConnectDB() (*gorm.DB, error) {
 	}
 
 	return db, nil
+}
+
+func AddUser(db *gorm.DB, username, password string) error {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	// Creating new user
+	user := User{
+		Username:      username,
+		Password_Hash: string(passwordHash),
+	}
+
+	if err := db.Create(&user).Error; err != nil {
+		return err
+	}
+	return nil
 }
